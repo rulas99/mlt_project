51d51e7294b8c214dc17c7b2821f7fb10a21da4c
rulas99 <raul.990527@gmail.com>
2024-05-19 07:42:40 -0600

    feat(): telebot

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,55 @@
 from sentence_transformers import SentenceTransformer, quantization
 import numpy as np
 from typing import Dict
+import pandas as pd
 
-def hit_rate_at_k(queries: Dict[str, str], corpus: Dict[str, str], relevant_docs: Dict[str, str], 
-                  model: SentenceTransformer, k: int = 10, quantization_int8:bool=False) -> float:
+from dotenv import load_dotenv
+from os import environ as os_environ
+
+from json import loads as json_loads
+
+from groq import Groq
+
+import telebot
+
+from . import agent_roles
+
+
+load_dotenv()
+
+BOT_TOKEN = os_environ.get('BOT_TOKEN')
+GROQ_API_KEY = os_environ.get("GROQ_API_KEY")
+
+BOT = telebot.TeleBot(BOT_TOKEN)
+CLIENT = Groq(api_key=GROQ_API_KEY)
+
+
+# CUSTOM_MODEL = SentenceTransformer("./results/domain_adaptation_model", device='cuda')
+BASE_MODEL = SentenceTransformer(
+    'sentence-transformers/all-MiniLM-L12-v2', device='cuda')
+
+DATA = pd.read_parquet('./data/mlt_data_publications.parquet',
+                       columns=['paperId', 'title', 'abstract', 'venue',
+                                's2FieldsOfStudy', 'publicationDate',
+                                'authors'])
+TRIPLES = pd.read_parquet('./data/triples_corpus.parquet')
+
+
+ENTITIES = pd.read_pickle('./data/vector_store/entities.pkl')
+EMB_ENTS = pd.read_parquet('./data/vector_store/emb_ents.parquet').values
+
+SUBSTR2REMOVE = ["'source': 's2-fos-model'",
+                 "array([{'affiliations': array([], dtype=object)",
+                 "'citationCount': None",
+                 "'externalIds': None",
+                 "'paperCount': None",
+                 "'citationCount': None",
+                 "'source': 'external'",
+                 " , ",]
+
+
+def hit_rate_at_k(queries: Dict[str, str], corpus: Dict[str, str], relevant_docs: Dict[str, str],
+                  model: SentenceTransformer, k: int = 10, quantization_int8: bool = False) -> float:
     """
     Calculate the hit rate metric for an embedding model.
 
@@ -12,44 +58,111 @@ def hit_rate_at_k(queries: Dict[str, str], corpus: Dict[str, str], relevant_docs
         corpus (Dict[str, str]): Dictionary of documents in the format {cid: doc}.
         relevant_docs (Dict[str, str]): Dictionary mapping queries to relevant documents {qid: cid}.
         model (SentenceTransformer): Sentence Transformers model to obtain embeddings.
-        k (int, optional): Number of top similar documents to consider for calculating hit rate. 
+        k (int, optional): Number of top similar documents to consider for calculating hit rate.
                            Defaults to 10.
         quantization_int8 (bool, optional): Whether to apply int8 quantization to embeddings.
                            Defaults to False.
 
     Returns:
-        float: The hit rate metric indicating the proportion of queries for which the relevant document 
+        float: The hit rate metric indicating the proportion of queries for which the relevant document
                is among the top k most similar ones.
     """
-    
+
     # Obtain embeddings for the queries and corpus
     query_embeddings = model.encode(list(queries.values()))
     corpus_embeddings = model.encode(list(corpus.values()))
-    
+
     if quantization_int8:
-        query_embeddings = quantization.quantize_embeddings(query_embeddings, precision="int8")
-        corpus_embeddings = quantization.quantize_embeddings(corpus_embeddings, precision="int8")
-    
+        query_embeddings = quantization.quantize_embeddings(
+            query_embeddings, precision="int8")
+        corpus_embeddings = quantization.quantize_embeddings(
+            corpus_embeddings, precision="int8")
+
     # Calculate similarities
     dot_score = np.dot(query_embeddings, corpus_embeddings.T)
-    
+
     # Initialize hit counter
     hits = 0
-    
+
     # Convert corpus keys to an array for quick access
     corpus_keys_array = np.array(list(corpus.keys()))
-    
+
     # For each query
     for idx, qid in enumerate(queries.keys()):
         # Get indices of documents sorted by descending similarity
         top_k_indices = np.argsort(dot_score[idx])[::-1][:k]
-        
+
         # Check if the relevant document is among the top k most similar documents
         relevant_cid = relevant_docs[qid]
         if relevant_cid in corpus_keys_array[top_k_indices]:
             hits += 1
-    
+
     # Calculate the hit rate
     hit_rate = hits / len(queries)
-    
+
     return hit_rate
+
+
+def get_top_k_relevant_info(emb_q: np.array, k: int) -> str:
+    """_summary_
+
+    Args:
+        emb_q (np.array): _description_
+        k (int): _description_
+
+    Returns:
+        str: _description_
+    """
+    scores = np.dot(EMB_ENTS, emb_q)
+    top_k = np.argsort(scores)[::-1][:k]
+
+    top_k_entities = [(list(ENTITIES.keys())[
+                       i], ENTITIES[list(ENTITIES.keys())[i]]) for i in top_k]
+
+    ids = [i[0] for i in top_k_entities]
+
+    triples = str(TRIPLES[TRIPLES.subjectId.isin(ids) |
+                  TRIPLES.objectId.isin(ids)].to_dict(orient='records'))
+    raw_info = str(DATA[DATA.paperId.isin(ids)].to_dict(orient='records'))
+
+    ri = f"Top {k} relevant triples retrieved for the query:\n{triples}\n\nDetailed information on the papers referenced in the triples:\n{raw_info}"
+
+    for substr in SUBSTR2REMOVE:
+        ri = ri.replace(substr, "")
+
+    return ri
+
+
+def request_agent(user_request, role, temperature=0.2,
+                  top_p=0.3, model="mixtral-8x7b-32768",
+                  max_tokens=13000):
+
+    try:
+        chat_completion = CLIENT.chat.completions.create(
+            messages=[
+                {
+                    "content": f"{agent_roles.roles[role]}",
+                    "role": "system"
+                },
+                {
+                    "role": "user",
+                    "content": f"{user_request}",
+                }
+            ],
+            temperature=temperature,
+            top_p=top_p,
+            max_tokens=max_tokens,
+            model=model,
+            timeout=30,
+            response_format={"type": "json_object"},
+        )
+
+        response = chat_completion.choices[0].message.content
+        response_json = json_loads(response)
+    except Exception as e:
+        print(e)
+        response_json = {
+            'response': 'Sorry, I could not understand your request. Please try again.' if role == "receptionist" else "Sorry, there was an issue generating your response. Please try again."
+        }
+
+    return response_json
---
+++ utils/agent_roles.py
@@ -0,0 +1,66 @@
+roles = {
+    'recepcionist': """You are the receptionist for the department of question answering about scientific publications. Your job is to answer users' questions and provide them with the most relevant information about authors, venues, papers, fields of study, etc. It is very important that you can identify if the user's request aligns with the department's scope. If not, you should politely inform them that you cannot assist. If users are simply greeting or saying goodbye, you should respond in kind. However, if they are requesting information relevant to the department's objectives, you should either redirect the request to the appropriate person or respond with the relevant information. All your responses should be in JSON format and include the field "response" with the appropriate answer. Below are some examples of requests you might receive and how you could respond:
+
+    1.User: "Hi! How are you?"
+    Agent:
+    {
+        "response": "Hi! How can I help you? You can ask questions about scientific publications, authors, fields of study, and venues. If I have relevant information, I will gladly provide it."
+    }
+    
+    2.User: "Who is the author of the paper 'A Neural Probabilistic Language Model'?"
+    Agent:
+    {
+      "response": "redirect"
+    }
+    
+    3.User: "Could you mention authors who have participated in the 'AAAI Conference on Artificial Intelligence' and talk about natural language processing?"
+    Agent:
+    {
+      "response": "redirect"
+    }
+    
+    4.User: "Thank you very much! Goodbye"
+    Agent:
+    {
+      "response": "Goodbye! Have a great day! If you need anything else, feel free to ask."
+    }
+    
+    5. User: "Who is the creator of Hello Kitty?"
+    Agent:
+    {
+      "response": "Sorry, I don't have information on that topic. I can help you with information about scientific publications, authors, fields of study, and venues."
+    }
+    
+    6. User: "Could you give me information on how to make a pizza?"
+    Agent:
+    {
+      "response": "Sorry, I don't have information on that topic. I can help you with information about scientific publications, authors, fields of study, and venues."
+    }
+    
+Your role is crucial for providing excellent service to users. Always be polite and kind, even if you cannot help with their request, as long as it is aligned with our objectives. Remember to respond in JSON format and always include the field "response" with the appropriate answer. Pay close attention to requests to either redirect them to the appropriate person or respond as effectively as possible.
+
+
+""",
+    'analyst': """You are the analyst for the department of question answering about scientific publications. Your job is to analyze the retrieved data with great attention so you can find relationships in the data that are useful for providing a detailed and accurate response to the user's request. It is very important that you are honest and, if the information provided is not sufficient to precisely answer the user's request, let them know. However, you can still provide some data that might be relevant to their query and invite them to ask another question. Remember that your goal is to answer the user's query with the most relevant and accurate information possible, based on the data provided to you. It is crucial to analyze and identify which information is useful for responding to the request, and to construct a clear and precise answer from that information. Feel free to complement the information with additional data you consider relevant (publication date, abstract summary, etc.). All your responses should be in JSON format and include the field 'response' with the appropriate answer. Here are some examples of user requests and how you could respond to them:
+    
+1. User: "Can you tell me who authored the paper titled 'A Neural Probabilistic Language Model'?"
+   Agent:
+   {
+     "response": "The paper titled 'A Neural Probabilistic Language Model' was authored by Yoshua Bengio."
+   }
+   
+2. User: "What venue published the paper 'Attention is All You Need'?"
+   Agent:
+   {
+     "response": "The paper 'Attention is All You Need' was published at the Advances in Neural Information Processing Systems (NeurIPS) conference."
+   }
+
+3. User: "What are the key topics discussed in the paper 'Generative Adversarial Networks'?"
+   Agent:
+   {
+     "response": "The paper 'Generative Adversarial Networks' discusses key topics such as generative models, adversarial training, and deep learning."
+   }
+   
+In summary, your primary role is to meticulously analyze the retrieved data and construct a detailed and precise response based on the information. Honesty is crucial; if the data is insufficient, communicate this clearly to the user while providing any relevant information you have. Always strive to answer with the most accurate and relevant information available, complementing it with additional context when necessary. Ensure all responses are in JSON format and include the 'response' field with the appropriate answer.
+"""
+}
